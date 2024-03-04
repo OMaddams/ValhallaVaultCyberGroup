e293c406bc9c97a2d446af0984ed4d7ef0748b8c
Alinia Andersson <alinia.andersson@edu.newton.se>
2024-03-04 12:20:26 +0100

    Made domain Models

---
+++ ValhallaVaultCyberGroup.Data/Models/Domain/CategoryModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ValhallaVaultCyberGroup.Data.Models.Domain
+{
+    public class CategoryModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = null!;
+        public List<SegmentModel> Segments { get; set; }
+
+    }
+}
---
+++ ValhallaVaultCyberGroup.Data/Models/Domain/QuestionModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ValhallaVaultCyberGroup.Data.Models.Domain
+{
+    public class QuestionModel
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Text { get; set; }
+        public List<ResponseModel> Responses { get; set; }
+
+        public int SubCategoryId { get; set; }
+        public int SubCategoryModel { get; set; }
+    }
+}
