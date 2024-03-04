5ce73b7d4b382cb16d129a259bcfc72e25c46a92
pelicanpubis <norah.binsalamah@hotmail.com>
2024-03-04 20:08:01 +0100

    Added a QuestionManager to handle logic

---
+++ ValhallaVaultCyberGroup.App/Managers/QuestionManager.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Dynamic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ValhallaVaultCyberGroup.Data.Models.Domain;
+
+namespace ValhallaVaultCyberGroup.App.Managers
+{
+    public class QuestionManager
+    {
+
+
+        //Hämta alla kategorier
+        
+        //public async Task <List<CategoryModel>> GetAllCategories()
+        //{
+          
+        //}
+
+    }
+}
