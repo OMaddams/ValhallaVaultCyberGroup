dee93653658a915a24e60d63009fbb7c3ec2d1d5
OMaddams <Omaddams@hotmail.com>
2024-03-04 19:13:53 +0100

    seed user and admin and role

--- ValhallaVaultCyberGroup.Ui/Program.cs
+++ ValhallaVaultCyberGroup.Ui/Program.cs
@@ -29,17 +29,72 @@ builder.Services.AddAuthentication(options =>
 var connectionString = builder.Configuration.GetConnectionString("DefaultConnection") ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseSqlServer(connectionString));
+
 builder.Services.AddDbContext<QuizDbContext>(options =>
     options.UseSqlServer(connectionString));
+
 builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 
 builder.Services.AddIdentityCore<ApplicationUser>(options => options.SignIn.RequireConfirmedAccount = true)
+    .AddRoles<IdentityRole>()
     .AddEntityFrameworkStores<ApplicationDbContext>()
     .AddEntityFrameworkStores<QuizDbContext>()
     .AddSignInManager()
     .AddDefaultTokenProviders();
 
 builder.Services.AddSingleton<IEmailSender<ApplicationUser>, IdentityNoOpEmailSender>();
+
+
+using (ServiceProvider serviceprovider = builder.Services.BuildServiceProvider())
+{
+    var context = serviceprovider.GetRequiredService<ApplicationDbContext>();
+    var signInManager = serviceprovider.GetRequiredService<SignInManager<ApplicationUser>>();
+    var roleManager = serviceprovider.GetRequiredService<RoleManager<IdentityRole>>();
+
+    context.Database.Migrate();
+
+    ApplicationUser newUser = new()
+    {
+        UserName = "user",
+        Email = "user@gmail.com",
+        EmailConfirmed = true,
+    };
+    ApplicationUser newAdmin = new()
+    {
+        UserName = "admin",
+        Email = "admin@gmail.com",
+        EmailConfirmed = true,
+    };
+
+
+    var user = signInManager.UserManager.FindByEmailAsync(newUser.Email).GetAwaiter().GetResult();
+    var admin = signInManager.UserManager.FindByEmailAsync(newAdmin.Email).GetAwaiter().GetResult();
+
+    if (user == null)
+    {
+        signInManager.UserManager.CreateAsync(newUser, "Password1234!").GetAwaiter().GetResult();
+
+
+    }
+    if (admin == null)
+    {
+        signInManager.UserManager.CreateAsync(newAdmin, "Password1234!").GetAwaiter().GetResult();
+
+        bool adminRoleExists = roleManager.RoleExistsAsync("Admin").GetAwaiter().GetResult();
+
+        if (!adminRoleExists)
+        {
+            IdentityRole adminRole = new()
+            {
+                Name = "Admin"
+            };
+            roleManager.CreateAsync(adminRole).GetAwaiter().GetResult();
+        }
+
+        signInManager.UserManager.AddToRoleAsync(newAdmin, "Admin").GetAwaiter().GetResult();
+    }
+}
+
 
 var app = builder.Build();
 
