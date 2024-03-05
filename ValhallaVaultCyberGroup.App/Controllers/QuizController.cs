570434c4d0ffbc11b0b4ba8c3195150cb0c0ffb6
pelicanpubis <norah.binsalamah@hotmail.com>
2024-03-05 17:22:07 +0100

    controller

--- ValhallaVaultCyberGroup.App/Controllers/QuizController.cs
+++ ValhallaVaultCyberGroup.App/Controllers/QuizController.cs
@@ -1,12 +1,53 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.Json.Serialization;
+using System.Text.Json;
 using System.Threading.Tasks;
+using ValhallaVaultCyberGroup.Data.Repositories;
+using ValhallaVaultCyberGroup.Data.Models.Domain;
 
 namespace ValhallaVaultCyberGroup.App.Controllers
 {
-    internal class QuizController
+    [Route("api/[controller]")]
+    [ApiController]
+    public class QuizController : ControllerBase
     {
+
+        private readonly IQuestionsRepo _questionsRepo;
+
+
+        private JsonSerializerOptions _jsonSerializerOptions = new()
+        {
+            ReferenceHandler = ReferenceHandler.Preserve // Konfigurering av JSON-serialisering
+        };
+
+
+        public QuizController(IQuestionsRepo questionsRepo)
+        {
+            _questionsRepo = questionsRepo;
+        }
+
+
+        //Get: api /Tickets
+        [HttpGet]
+
+        public async Task <IActionResult> GetAllQuestionsAsync()
+        {
+            List<QuestionModel> allQuestions = await _questionsRepo.GetAllQuestionsAsync(); 
+            if (allQuestions == null) 
+            { 
+                return NotFound();
+            }
+
+            else
+            {
+                var questionsJson = JsonSerializer.Serialize(allQuestions, _jsonSerializerOptions);
+                return Ok(questionsJson);
+            }
+        }
+
     }
 }
---
+++ ValhallaVaultCyberGroup.App/Services/QuestionService.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ValhallaVaultCyberGroup.App.Services
+{
+    public class QuestionService
+    {
+
+        public HttpClient Client { get; set; } = new()
+        {
+            BaseAddress = new Uri("https://localhost:7191/api/") 
+        };
+
+       
+    }
+}
