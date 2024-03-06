1b6fcd25a6d7448aca4e82de54f68fbaadb0a20c
Izabell <izabell.hallgren@gmail.com>
2024-03-06 10:14:10 +0100

    Made changes to the UI on the 'Register' page

---
+++ ValhallaVaultCyberGroup.App/Controllers/QuizController.cs
@@ -0,0 +1,216 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Text.Json;
+using System.Threading.Tasks;
+using ValhallaVaultCyberGroup.Data.Repositories;
+using ValhallaVaultCyberGroup.Data.Models.Domain;
+
+namespace ValhallaVaultCyberGroup.App.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class QuizController : ControllerBase
+    {
+
+        private readonly IQuestionsRepo _questionsRepo;
+
+
+        private JsonSerializerOptions _jsonSerializerOptions = new()
+        {
+            ReferenceHandler = ReferenceHandler.Preserve // Konfigurering av JSON-serialisering
+        };
+
+
+        public QuizController(IQuestionsRepo questionsRepo)
+        {
+            _questionsRepo = questionsRepo;
+        }
+
+
+        //Get: api /Tickets
+        [HttpGet("Questions")]
+
+        public async Task <IActionResult> GetAllQuestionsAsync()
+        {
+            List<QuestionModel> allQuestions = await _questionsRepo.GetAllQuestionsAsync(); 
+            if (allQuestions == null) 
+            { 
+                return NotFound();
+            }
+
+            else
+            {
+                var questionsJson = JsonSerializer.Serialize(allQuestions, _jsonSerializerOptions);
+                return Ok(questionsJson);
+            }
+        }
+
+
+        [HttpGet("SingleQuestion/{id}")]
+
+        public async Task<IActionResult> GetQuestionAsync(int id)
+        {
+            QuestionModel? question = await _questionsRepo.GetQuestionAsync(id);
+       if(question == null)
+            {
+                return NotFound();
+            }
+
+       else
+            {
+                var questionJson = JsonSerializer.Serialize(question, _jsonSerializerOptions);
+                return Ok(questionJson);
+            }
+        
+        }
+
+
+        //Get: api /Tickets
+        [HttpGet("Responses")]
+
+        public async Task<IActionResult> GetAllResponsesAsync()
+        {
+            List<ResponseModel> allResponses = await _questionsRepo.GetAllResponsesAsync();
+            if (allResponses == null)
+            {
+                return NotFound();
+            }
+
+            else
+            {
+                var responsesJson = JsonSerializer.Serialize(allResponses, _jsonSerializerOptions);
+                return Ok(responsesJson);
+            }
+        }
+
+      
+        [HttpGet("SingleResponse/{id}")]
+
+        public async Task<IActionResult> GetResponseAsync(int id)
+        {
+            ResponseModel? response = await _questionsRepo.GetResponseAsync(id);
+            if (response == null)
+            {
+                return NotFound();
+            }
+
+            else
+            {
+                var responseJson = JsonSerializer.Serialize(response, _jsonSerializerOptions);
+                return Ok(responseJson);
+            }
+
+        }
+
+
+        [HttpGet("Categories")]
+        public async Task<IActionResult> GetAllCategoriesAsync()
+        {
+            List<CategoryModel> allCategories = await _questionsRepo.GetAllAsync();
+            if (allCategories == null)
+            {
+                return NotFound();
+            }
+
+            else
+            {
+                var categoriesJson = JsonSerializer.Serialize(allCategories, _jsonSerializerOptions);
+                return Ok(categoriesJson);
+            }
+        }
+
+
+        [HttpGet("SingleCategory/{id}")]
+
+        public async Task<IActionResult> GetCategoryAsync(int id)
+        {
+            CategoryModel? category = await _questionsRepo.GetCategoryAsync(id);
+            if (category == null)
+            {
+                return NotFound();
+            }
+
+            else
+            {
+                var categoryJson = JsonSerializer.Serialize(category, _jsonSerializerOptions);
+                return Ok(categoryJson);
+            }
+
+        }
+
+        [HttpGet("Segments")]
+        public async Task<IActionResult> GetAllSegmentAsync()
+        {
+            List<SegmentModel> allSegments = await _questionsRepo.GetAllSegmentsAsync();
+            if (allSegments == null)
+            {
+                return NotFound();
+            }
+
+            else
+            {
+                var segmentsJson = JsonSerializer.Serialize(allSegments, _jsonSerializerOptions);
+                return Ok(segmentsJson);
+            }
+        }
+
+
+        [HttpGet("SingleSegment/{id}")]
+
+        public async Task<IActionResult> GetSegmentAsync(int id)
+        {
+            SegmentModel? segment = await _questionsRepo.GetSegmentAsync(id);
+            if (segment == null)
+            {
+                return NotFound();
+            }
+
+            else
+            {
+                var segmentJson = JsonSerializer.Serialize(segment, _jsonSerializerOptions);
+                return Ok(segmentJson);
+            }
+
+        }
+
+        [HttpGet("SubCategories")]
+        public async Task<IActionResult> GetAllSubCategoriesAsync()
+        {
+            List<SubCategoryModel> allSubCategories = await _questionsRepo.GetAllSubCategoriesAsync();
+            if (allSubCategories == null)
+            {
+                return NotFound();
+            }
+
+            else
+            {
+                var subCategoriesJson = JsonSerializer.Serialize(allSubCategories, _jsonSerializerOptions);
+                return Ok(subCategoriesJson);
+            }
+        }
+
+
+        [HttpGet("SingleSubCategory/{id}")]
+
+        public async Task<IActionResult> GetSubCategoryAsync(int id)
+        {
+            SubCategoryModel? subCategory = await _questionsRepo.GetSubCategoryAsync(id);
+            if (subCategory == null)
+            {
+                return NotFound();
+            }
+
+            else
+            {
+                var subCategoryJson = JsonSerializer.Serialize(subCategory, _jsonSerializerOptions);
+                return Ok(subCategoryJson);
+            }
+
+        }
+
+    }
+}
--- ValhallaVaultCyberGroup.Ui/Program.cs
+++ ValhallaVaultCyberGroup.Ui/Program.cs
@@ -2,6 +2,7 @@ using Microsoft.AspNetCore.Components.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using ValhallaVaultCyberGroup.App.Managers;
+using ValhallaVaultCyberGroup.App.Services;
 using ValhallaVaultCyberGroup.Data.Data;
 using ValhallaVaultCyberGroup.Data.Repositories;
 using ValhallaVaultCyberGroup.Ui.Components;
@@ -11,8 +12,24 @@ using ValhallaVaultCyberGroup.Ui.Data;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddControllers();
+
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddCors(options =>
+{
+	options.AddPolicy("AllowAll", options =>
+	{
+		options.AllowAnyHeader();
+		options.AllowAnyMethod();
+		options.AllowAnyOrigin();
+	});
+});
+
 builder.Services.AddRazorComponents()
-    .AddInteractiveServerComponents();
+	.AddInteractiveServerComponents();
 
 builder.Services.AddCascadingAuthenticationState();
 builder.Services.AddScoped<IdentityUserAccessor>();
@@ -21,99 +38,112 @@ builder.Services.AddScoped<AuthenticationStateProvider, IdentityRevalidatingAuth
 builder.Services.AddScoped<IQuestionsRepo, QuestionsRepo>();
 builder.Services.AddScoped<QuestionManager>();
 
+
+
+
 builder.Services.AddAuthentication(options =>
-    {
-        options.DefaultScheme = IdentityConstants.ApplicationScheme;
-        options.DefaultSignInScheme = IdentityConstants.ExternalScheme;
-    })
-    .AddIdentityCookies();
+	{
+		options.DefaultScheme = IdentityConstants.ApplicationScheme;
+		options.DefaultSignInScheme = IdentityConstants.ExternalScheme;
+	})
+	.AddIdentityCookies();
 
 var connectionString = builder.Configuration.GetConnectionString("DefaultConnection") ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
-    options.UseSqlServer(connectionString));
+	options.UseSqlServer(connectionString));
 
 var quizConnectionString = builder.Configuration.GetConnectionString("QuizConnection");
 builder.Services.AddDbContext<QuizDbContext>(options =>
-    options.UseSqlServer(quizConnectionString));
+	options.UseSqlServer(quizConnectionString));
 
 builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 
 builder.Services.AddIdentityCore<ApplicationUser>(options => options.SignIn.RequireConfirmedAccount = true)
-    .AddRoles<IdentityRole>()
-    .AddEntityFrameworkStores<ApplicationDbContext>()
-    .AddSignInManager()
-    .AddDefaultTokenProviders();
+	.AddRoles<IdentityRole>()
+	.AddEntityFrameworkStores<ApplicationDbContext>()
+	.AddSignInManager()
+	.AddDefaultTokenProviders();
 
 builder.Services.AddSingleton<IEmailSender<ApplicationUser>, IdentityNoOpEmailSender>();
 
 
+
 using (ServiceProvider serviceprovider = builder.Services.BuildServiceProvider())
 {
-    var context = serviceprovider.GetRequiredService<ApplicationDbContext>();
-    var signInManager = serviceprovider.GetRequiredService<SignInManager<ApplicationUser>>();
-    var roleManager = serviceprovider.GetRequiredService<RoleManager<IdentityRole>>();
+	var context = serviceprovider.GetRequiredService<ApplicationDbContext>();
+	var signInManager = serviceprovider.GetRequiredService<SignInManager<ApplicationUser>>();
+	var roleManager = serviceprovider.GetRequiredService<RoleManager<IdentityRole>>();
 
-    context.Database.Migrate();
+	context.Database.Migrate();
 
-    ApplicationUser newUser = new()
-    {
-        UserName = "user@gmail.com",
-        Email = "user@gmail.com",
-        EmailConfirmed = true,
-    };
-    ApplicationUser newAdmin = new()
-    {
-        UserName = "admin@gmail.com",
-        Email = "admin@gmail.com",
-        EmailConfirmed = true,
-    };
-
-
-    var user = signInManager.UserManager.FindByEmailAsync(newUser.Email).GetAwaiter().GetResult();
-    var admin = signInManager.UserManager.FindByEmailAsync(newAdmin.Email).GetAwaiter().GetResult();
-
-    if (user == null)
-    {
-        signInManager.UserManager.CreateAsync(newUser, "Password1234!").GetAwaiter().GetResult();
+	ApplicationUser newUser = new()
+	{
+		UserName = "user@gmail.com",
+		Email = "user@gmail.com",
+		EmailConfirmed = true,
+	};
+	ApplicationUser newAdmin = new()
+	{
+		UserName = "admin@gmail.com",
+		Email = "admin@gmail.com",
+		EmailConfirmed = true,
+	};
 
 
-    }
-    if (admin == null)
-    {
-        signInManager.UserManager.CreateAsync(newAdmin, "Password1234!").GetAwaiter().GetResult();
+	var user = signInManager.UserManager.FindByEmailAsync(newUser.Email).GetAwaiter().GetResult();
+	var admin = signInManager.UserManager.FindByEmailAsync(newAdmin.Email).GetAwaiter().GetResult();
 
-        bool adminRoleExists = roleManager.RoleExistsAsync("Admin").GetAwaiter().GetResult();
+	if (user == null)
+	{
+		signInManager.UserManager.CreateAsync(newUser, "Password1234!").GetAwaiter().GetResult();
 
-        if (!adminRoleExists)
-        {
-            IdentityRole adminRole = new()
-            {
-                Name = "Admin"
-            };
-            roleManager.CreateAsync(adminRole).GetAwaiter().GetResult();
-        }
 
-        signInManager.UserManager.AddToRoleAsync(newAdmin, "Admin").GetAwaiter().GetResult();
-    }
+	}
+	if (admin == null)
+	{
+		signInManager.UserManager.CreateAsync(newAdmin, "Password1234!").GetAwaiter().GetResult();
+
+		bool adminRoleExists = roleManager.RoleExistsAsync("Admin").GetAwaiter().GetResult();
+
+		if (!adminRoleExists)
+		{
+			IdentityRole adminRole = new()
+			{
+				Name = "Admin"
+			};
+			roleManager.CreateAsync(adminRole).GetAwaiter().GetResult();
+		}
+
+		signInManager.UserManager.AddToRoleAsync(newAdmin, "Admin").GetAwaiter().GetResult();
+	}
 }
 
 
-// Add MVC services
-builder.Services.AddControllers();
+
 
 var app = builder.Build();
+
+
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.UseMigrationsEndPoint();
+	app.UseMigrationsEndPoint();
 }
 else
 {
-    app.UseExceptionHandler("/Error", createScopeForErrors: true);
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-    app.UseHsts();
+	app.UseExceptionHandler("/Error", createScopeForErrors: true);
+	// The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+	app.UseHsts();
 }
+
+//// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+	app.UseSwagger();
+	app.UseSwaggerUI();
+}
+
 
 app.UseHttpsRedirection();
 
@@ -121,16 +151,12 @@ app.UseStaticFiles();
 app.UseAntiforgery();
 
 app.MapRazorComponents<App>()
-    .AddInteractiveServerRenderMode();
+	.AddInteractiveServerRenderMode();
 
 // Add additional endpoints required by the Identity /Account Razor components.
 app.MapAdditionalIdentityEndpoints();
 
-//app.UseEndpoints(endpoints =>
-//{
-//    endpoints.MapControllers(); // Lägger till controllers som endpoints
-//    endpoints.MapRazorComponents<App>().AddInteractiveServerRenderMode();
-//    endpoints.MapAdditionalIdentityEndpoints();
-//});
+app.MapControllers();
+app.UseCors("AllowAll");
 
 app.Run();
