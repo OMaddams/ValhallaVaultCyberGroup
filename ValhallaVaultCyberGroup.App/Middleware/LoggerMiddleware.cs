91377d2f1bbcaa7a4649b78228dbad0e77eb8551
OMaddams <Omaddams@hotmail.com>
2024-03-13 19:14:00 +0100

    minimal request/response logger

---
+++ ValhallaVaultCyberGroup.App/Middleware/LoggerMiddleware.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+
+namespace ValhallaVaultCyberGroup.App.Middleware
+{
+    public class LoggerMiddleware
+    {
+        private readonly RequestDelegate next;
+        private readonly ILogger logger;
+
+        public LoggerMiddleware(RequestDelegate next, ILogger logger)
+        {
+            this.next = next;
+            this.logger = logger;
+        }
+
+        public async Task InvokeAsync(HttpContext context)
+        {
+            logger.LogInformation($"Request {context.Request.Path} {context.Request.Cookies}");
+
+
+            await next(context);
+
+            logger.LogInformation($"Response {context.Response.StatusCode}");
+        }
+    }
+}
